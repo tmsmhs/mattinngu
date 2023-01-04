bf5debd215a39838493d5f5acc8750c15ec62c53
Gobousei <93245017+Gobousei@users.noreply.github.com>
2023-01-04 23:44:44 +0900

    Add files via upload

---
+++ beta3/judge.js
@@ -0,0 +1,57 @@
+function judges(){//結果をジャッジする
+    var query = location.search;
+    var value = query.split('=');
+    var names =decodeURIComponent(value[1]);
+    var array = names.split(',');
+    console.log(array);
+    if(array.length<3){
+        return 'false';
+    }else{
+  var cool=0;
+  var cute=0;
+  var feminine=0;
+  var fresh=0;
+  var typearr =[['クールタイプ','キュートタイプ','フレッシュタイプ','フェミニンタイプ'],['cool','cute','fresh','feminine']];
+  for (var i=0;i<array.length;i++){
+    switch(array[i]){
+      case 'cool':var cool = cool+1;
+      break
+      case 'cute':var cute = cute+1;
+      break
+      case 'feminine':var feminine = feminine+1;
+      break
+      case 'fresh':var fresh = fresh+1;
+      break
+    }
+    }
+    var resnum = [cool,cute,fresh,feminine];
+    console.log(resnum);
+    var max = Math.max.apply(null,resnum);
+    console.log(max);
+    if(max<2){
+        return 'false';
+    }else{
+        return 'true';
+    }
+}
+  }
+
+  function page(type){
+    let judge = judges();
+    if(judge=='true'){
+        var query = location.search;
+        var value = query.split('=');
+          return 'answer.html?name=' +decodeURIComponent(value[1])+','+type;
+    }else{
+          var query = location.search;
+        var value = query.split('=');
+        var first =decodeURIComponent(value[1]);
+        if(first=='undefined'){
+            return 'index.html?name='+type;
+        }else{
+      return 'index.html?name=' +decodeURIComponent(value[1])+','+type;
+        }
+    }
+  }
+
+  
