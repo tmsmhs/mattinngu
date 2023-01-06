d087422c3b34ee1dd6128130712377e573284877
Gobousei <93245017+Gobousei@users.noreply.github.com>
2023-01-06 10:59:59 +0900

    Add files via upload

--- beta3/judge.js
+++ beta3/judge.js
@@ -1,5 +1,8 @@
+//設定とかいろいろ
 var answers =[];
 var restoration=[];
+var placeb =['左上','右上','左下','右下'];
+var arrb =['cool','cute','fresh','feminine'];
 function judges(test){//結果をジャッジする
     var array = answers;
     array.push(test);
@@ -11,7 +14,6 @@ function judges(test){//結果をジャッジする
   var cute=0;
   var feminine=0;
   var fresh=0;
-  var typearr =[['クールタイプ','キュートタイプ','フレッシュタイプ','フェミニンタイプ'],['cool','cute','fresh','feminine']];
   for (var i=0;i<array.length;i++){
     switch(array[i]){
       case 'cool':var cool = cool+1;
@@ -27,7 +29,6 @@ function judges(test){//結果をジャッジする
     var resnum = [cool,cute,fresh,feminine];
     console.log(resnum);
     var max = Math.max.apply(null,resnum);
-    console.log(max);
     if(max<2){
         return 'false';
     }else{
@@ -51,5 +52,31 @@ function judges(test){//結果をジャッジする
     }
   }
 
+  //imgのシャッフル
+function shuffle(){
+  document.getElementById("js").style.display ="none";
+  var list = lists();
+  var place =placeb.concat();
+  var arr =arrb.concat();
+  var li =arrb.concat();
+  for(let t=0;t<li.length;t++){
+    let rondom = Math.floor( Math.random() * arr.length );
+    let type = arr[rondom];
+    let listlen = list[rondom][0].length+1;
+    let a = Math.floor( Math.random() * listlen );
+    let num = li.indexOf(type);
+    let reimg = list[num][a][1];
+    let id = t+1;
+    let img = document.getElementById('image'+id);
+    img.src = type+'/'+reimg;
+    let rehum = list[num][a][0];
+    document.getElementById('p'+id).innerText = place[t]+':'+rehum;
+    let onc = document.getElementById('a'+id);
+    onc.id=type;
+    arr.splice(rondom,1);
+    restoration.push(type);
+    console.log(type);
+  }
+}
 
   
